3a8a95a2cd8a8af4d096b415a22ec2fb8e347981
Jérémie LIECHTI <liechti.jeremie@sfr.fr>
2015-04-26 03:20:56 +0200

    Modify rental's controller
    Add comments
    Start coding

---
+++ controllers/rentals/ModifyRental_Controller.php
@@ -0,0 +1,16 @@
+<?php
+	
+	/*
+	 * Controller for rental's modifications
+	 * This class manages the modifications on rentals
+	 *
+	 * @author Jérémie LIECHTI
+	 * @version 0.0.1
+	 * @copyright 2015 3iL
+	 */
+
+	public class ModifyRental extends RentalController {
+		
+		
+		
+	}
---
+++ controllers/rentals/Rental_Controller.php
@@ -0,0 +1,16 @@
+<?php
+	
+	/*
+	 * Rentals controller core
+	 * This class is above all rentals classes
+	 *
+	 * @author Jérémie LIECHTI
+	 * @version 0.0.1
+	 * @copyright 2015 3iL
+	 */
+
+	public class RentalController {
+		
+		
+		
+	}
