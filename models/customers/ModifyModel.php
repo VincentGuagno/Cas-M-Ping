38e542964f1c2b445b31ad455330e75c79dcfc27
Jérémie LIECHTI <liechti.jeremie@sfr.fr>
2015-05-05 19:40:17 +0200

    Database refactoring
    Refactore modifyModel

--- models/customers/ModifyModel.php
+++ models/customers/ModifyModel.php
@@ -2,33 +2,47 @@
 
 <?php
 
-	/**********************     REQUEST     *********************/
-	/** Application access database                                           
-	/************************************************************/
+	/*
+	 * Model for customer modifications
+	 * This class handles the modification of a customer
+	 *
+	 * @author Bastien VAUTIER
+	 * @version 0.0.1
+	 * @copyright 2015 3iL
+	 */
 
 	class modifyModel {
 
-
-	
-		/***
-		*  modify all customer's informations from one customer 
+		/**
+		* modify all customer's informations from one customer 
 		*
+		* @param cust_id, customer's id
+		* @param cust_name, customer's name
+		* @param cust_address, customer's address
+		* @param cust_zipCode, customer's zip code
+		* @param cust_city, customer's city
+		* @param cust_phoneNumber, customer's phone number
+		* @param cust_recordNumber, customer's record number
+		*
+		* @return 0 without errors, exception message any others cases
 		*/
-		public function modify_model($cust_id, $cust_name, $cust_address, $cust_postal_code, $cust_city, $cust_phone_number, $cust_record_number) {
+		public function modify_model($cust_id, $cust_name, $cust_address, $cust_zipCode, $cust_city, $cust_phoneNumber, $cust_addNumber) {
 			try {
-				$model = $this->db->prepare('UPDATE camping.customer SET cust_name = "?",cust_address = "?",cust_postal_code = "?",cust_city ="?", cust_phone_number = "?",cust_record_number = "?"  WHERE cust_id = ?');
-				$model->bindValue(1, $cust_name, PDO::PARAM_STR);
-				$model->bindValue(2, $cust_address, PDO::PARAM_STR);
-				$model->bindValue(3, $cust_postal_code, PDO::PARAM_STR);
-				$model->bindValue(4, $cust_city, PDO::PARAM_STR);
-				$model->bindValue(5, $cust_phone_number, PDO::PARAM_STR);
-				$model->bindValue(6, $cust_record_number, PDO::PARAM_STR);
-				//pas sure de pouvoir modifier son nom vue que je prens 2 fois le mêmes paramètre.
-				$model->bindValue(7, $cust_id, PDO::PARAM_INT);
-				$model->execute();
-				$model->closeCursor();
+				$qry = $this->db->prepare('UPDATE camping.customer SET cust_name =?, cust_address =?, cust_postal_code =?, cust_city =?, cust_phone_number =?, cust_record_number =? WHERE cust_id =?');
+				$qry->bindValue(1, $cust_name, PDO::PARAM_STR);
+				$qry->bindValue(2, $cust_address, PDO::PARAM_STR);
+				$qry->bindValue(3, $cust_zipCode, PDO::PARAM_STR);
+				$qry->bindValue(4, $cust_city, PDO::PARAM_STR);
+				$qry->bindValue(5, $cust_phoneNumber, PDO::PARAM_STR);
+				$qry->bindValue(6, $cust_addNumber, PDO::PARAM_STR);
+				$qry->bindValue(7, $cust_id, PDO::PARAM_INT);
+				$qry->execute();
+				$qry->closeCursor();
+				
+				return 0;
+			} catch(Exception $e) {
+				return $e->getMessage();
 			}
-			/catch(Exception $e) {header('Location: ./erreur/500');}
 		}
 
 	}
