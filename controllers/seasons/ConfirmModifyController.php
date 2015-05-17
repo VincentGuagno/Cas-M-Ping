f8265945ba00e5688b3493bc354b7f88db5586c8
Jérémie LIECHTI <liechti.jeremie@sfr.fr>
2015-05-17 03:49:04 +0200

    Create ConfirmModifyController.php

---
+++ controllers/seasons/ConfirmModifyController.php
@@ -0,0 +1,103 @@
+<?php
+
+	/*
+	 * Controller for confirm season modifications
+	 * This class handles modifications seasons
+	 *
+	 * @author Jérémie LIECHTI
+	 * @version 0.0.1
+	 * @copyright 2015 3iL
+	 */
+
+	require_once('SeasonController.php');
+	 
+	class ConfirmModifyController extends SeasonController {
+		
+		/**
+		 * Name of called model
+		 */
+		private $model_name = 'Modify';
+		
+		/**
+		 * Name of called view
+		 */
+		private $view_name = 'modify';
+		
+		/**
+		 * The constructor of ConfirmModifyController
+		 */
+		public function __construct() {
+			try {
+				$this->init();
+			} catch (Exception $e) {
+				echo $e->getMessage();
+			}
+		}
+		
+		/**
+		 * Initialize the ConfirmModifyController class and their parents
+		 */
+		public function init() {
+			try {
+				parent::init();	
+			} catch (Exception $e) {
+				throw new Exception('Une erreur est survenue durant le chargement du module: '.$e->getMessage());
+			}
+			
+			if (file_exists (_CONTROLLERS_DIR_ .'/Tools.php')) {
+				$url = Tools::getInstance()->request_url;
+				$controller = Tools::getInstance()->getUrl_controller($url);
+				
+				if ($controller == 'ConfirmModifyController') {
+					if (file_exists (_SEASONS_MODELS_ .'/'. $this->model_name .'Model.php')) {			
+						try {	
+							require_once (_SEASONS_MODELS_ .'/'. $this->model_name .'Model.php');
+							$id = Tools::getInstance()->getUrl_id($url);
+							
+							$datetime1 = new DateTime($_POST['beginDate']);
+							$datetime2 = new DateTime($_POST['endDate']);
+							$interval = $datetime1->diff($datetime2);
+							
+							Tools::getInstance()->createPost($_POST);
+							
+							if(!empty($_POST['name']) && !empty($_POST['beginDate']) && !empty($_POST['endDate']) && !empty($_POST['coefficient'])) {
+								
+								$_POST['coefficient'] = ($_POST['coefficient'] > 1) ? 1 : $_POST['coefficient'];
+								$_POST['coefficient'] = ($_POST['coefficient'] <= 0) ? 0.1 : $_POST['coefficient'];
+								\Season\ModifyModel::getInstance()->modify_season($id, $_POST['name'], $_POST['beginDate'], $_POST['endDate'], $_POST['coefficient']);
+								header('Location: /Cas-M-Ping/seasons/show/'.$id);
+								
+							} else {
+								header('Location: /Cas-M-Ping/seasons/modify/'.$id);
+							}
+
+						} catch (Exception $e) {
+							throw new Exception('Une erreur est survenue durant la modification des données: '.$e->getMessage());
+						}
+					} else {
+						throw new Exception('Le modèle "'. $this->model_name .'" n\'existe pas dans "'._SEASONS_MODELS_ .'"!');
+					}
+				} else {
+					throw new Exception('Une erreur est survenue durant la phase de routage!');
+				}
+			} else {
+				throw new Exception('L\'URL n\'est pas évaluable!');
+			}
+		}
+		
+		/**
+	     * @see SeasonController::checkAccess()
+	     * @return true if the controller is available for the current user/visitor, false any other cases
+	     */
+	    public function checkAccess() {
+			return true;
+	    }
+
+		/**
+		 * @see SeasonController::viewAccess()
+		 * @return true if the current user/visitor has valid view permissions, false any other cases
+		 */
+		public function viewAccess() {
+			return true;
+		}		
+	}
