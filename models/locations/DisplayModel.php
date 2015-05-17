5aefed71809310228fb301ec977c585e330d2dcb
Bastien Vautier <bvbastienvautier@gmail.com>
2015-05-17 20:21:56 +0200

    [model][location] add type location

--- models/locations/DisplayModel.php
+++ models/locations/DisplayModel.php
@@ -156,6 +156,74 @@
 			}
 		}
 
+		/**
+		 * Display all TypeLocationModel's informations from one TypeLocationModel 
+		 *
+	     * @return return_qry : result into an object, exception message any others cases
+		 */
+		public function display_TypeLocationModels() {
+			try {
+				$qry = $this->db->prepare('SELECT * FROM type_location ORDER BY type_location_id');				
+				$qry->execute();
+				//put  the result into an object
+				$return_qry = $qry->fetchAll();
+				$qry->closeCursor();
+				return $return_qry;
+				
+			} catch(Exception $e) {
+				return $e->getMessage();
+			}
+		}
+
+		/**
+		 * return customer's id
+		 *	
+		 * @return return_qry : result into an object, exception message any others cases
+		 */
+		public function display_TypeLocationModel($cust_id) {
+			try {
+				$qry = $this->db->prepare('SELECT * FROM type_location WHERE type_location.type_location_id  =?');	
+                        								
+				$qry->bindValue(1, $cust_id, \PDO::PARAM_STR);				
+
+				$qry->execute();
+				//get customer's ID      put  the result into an object
+				$return_qry = $qry->fetchAll();
+
+				$qry->closeCursor();
+				return $return_qry;
+			} catch(Exception $e) {
+				return $e->getMessage();
+			}
+		}
+
+		/**
+		 * return customer's id
+		 *
+		 * @param cust_firstName, customer's name
+		 * @param cust_lastName, customer's name
+		 * @return return_qry : result into an object, exception message any others cases
+		 */
+		public function get_TpyeLocationId($cust_firstName, $cust_lastName) {
+			try {
+	
+				$qry = $this->db->prepare('SELECT customer.cust_id FROM	customer
+                        					WHERE customer.cust_firstName =? and customer.cust_lastName =?');	
+                        								
+				$qry->bindValue(1, $cust_firstName, \PDO::PARAM_STR);
+				$qry->bindValue(2, $cust_lastName, \PDO::PARAM_STR);
+
+				$qry->execute();
+					//put  the result into an object
+				$return_qry = $qry->fetchAll();
+				$qry->closeCursor();
+				return $return_qry;
+
+			} catch(Exception $e) {
+				return $e->getMessage();
+			}
+		}
+
 	}
 
 ?>
