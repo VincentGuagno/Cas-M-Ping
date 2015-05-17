fe833d49a9175074ef28177c1f41dce4df864807
Bastien Vautier <bvbastienvautier@gmail.com>
2015-05-17 18:40:03 +0200

    [model][customer][add_model]  with uuid()

--- models/customers/AddModel.php
+++ models/customers/AddModel.php
@@ -69,16 +69,14 @@
 		 * @param cust_address, customer's address
 		 * @param cust_zipCode, customer's zip code
 		 * @param cust_city, customer's city
-		 * @param cust_phoneNumber, customer's phone number
-		 * @param cust_recordNumber, customer's record number
+		 * @param cust_phoneNumber, customer's phone number		
 		 * @return 0 without errors, exception message any others cases
 		 */
 		public function add_customer($cust_lastName,
 									 $cust_address,
 									 $cust_zipCode, 
 									 $cust_city, 
-									 $cust_phoneNumber, 
-									 $cust_recordNumber,
+									 $cust_phoneNumber, 									
 									 $cust_firstName) {
 			try {
 				$qry = $this->db->prepare('INSERT INTO camping.customer (cust_lastName,
@@ -87,13 +85,12 @@
 																		 cust_city, 
 																		 cust_phone_number, 
 																		 cust_record_number, 
-																		 cust_firstName) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?)');
+																		 cust_firstName) VALUES (NULL, ?, ?, ?, ?, ?, UUID(), ?)');
 				$qry->bindValue(1, $cust_lastName, \PDO::PARAM_STR);
 				$qry->bindValue(2, $cust_address, \PDO::PARAM_STR);
 				$qry->bindValue(3, $cust_zipCode, \PDO::PARAM_STR);
 				$qry->bindValue(4, $cust_city, \PDO::PARAM_STR);
-				$qry->bindValue(5, $cust_phoneNumber, \PDO::PARAM_STR);
-				$qry->bindValue(6, $cust_recordNumber, \PDO::PARAM_STR);
+				$qry->bindValue(5, $cust_phoneNumber, \PDO::PARAM_STR);			
 				$qry->bindValue(6, $cust_firstName, \PDO::PARAM_STR);
 				$qry->execute();
 				$qry->closeCursor();
